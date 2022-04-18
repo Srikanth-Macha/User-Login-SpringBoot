21b76820c0611ebe0fd1a178eb7c6d4ffdfa8c28
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-04-18 18:34:53 +0530

    Remote connectivity configuration

--- src/main/kotlin/com/example/userauthentication/service/UserService.kt
+++ src/main/kotlin/com/example/userauthentication/service/UserService.kt
@@ -2,14 +2,11 @@ package com.example.userauthentication.service
 
 import com.example.userauthentication.models.User
 import com.example.userauthentication.repository.UserRepo
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class UserService {
-    private val logger: Logger = LoggerFactory.getLogger(UserService::class.java)
 
     @Autowired
     private lateinit var userRepo: UserRepo
@@ -19,11 +16,10 @@ class UserService {
     fun getUsers(): List<User> = userRepo.findAll()
 
     fun findByUsername(requestUser: User): User? {
-        val username = requestUser.username
         val usersList: List<User> = getUsers()
 
         for (user in usersList)
-            if (user.username == username)
+            if (user.username == requestUser.username && user.password == requestUser.password)
                 return user
 
         return null
